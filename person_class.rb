87b248369012090c6d4982e60d188e1cfd85601a
Tracy-miranja <barazatracy44@gmail.com>
2023-05-22 16:59:51 +0300

    Add getters

--- person_class.rb
+++ person_class.rb
@@ -1,1 +1,22 @@
+class person
+    def initialize(name="Unknown", age, parent_permission=true)
+        @id = id
+        @name = name
+        @age = age
+    end
 
+    def id
+        @id
+    end
+    def name
+        @name
+    end
+
+    def name=(value)
+        @name=value
+    end
+
+    def age
+        @age
+    end
+end
