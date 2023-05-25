3ccb022fc92717c4b4854b3013217783713b6727
Tracy-miranja <barazatracy44@gmail.com>
2023-05-25 14:44:57 +0300

    Impliment changes

--- main.rb
+++ main.rb
@@ -1,17 +1,15 @@
 require_relative './app'
 
-def run_option
-  puts 'Welcome to the Library App!'
-  puts 'Please choose an option:'
-  puts '1 - List all books.'
-  puts '2 - List all people'
-  puts '3 - Create a person'
-  puts '4 - Create a book'
-  puts '5 - Create a rental'
-  puts '6 - List all rentals for a given person ID'
-  puts '7 - Quit'
+def print_menu
+  puts "\nPlease choose an option:"
+  puts '1. List all books'
+  puts '2. List all people'
+  puts '3. Create a person'
+  puts '4. Create a book'
+  puts '5. Create a rental'
+  puts '6. List all rentals for a person'
+  puts '7. Quit'
 end
-
 def create_person(app)
   puts 'Do you want to create a student(1) or a teacher(2)? [Input the number]:-'
   option = gets.chomp
@@ -36,41 +34,46 @@ def create_person(app)
   end
 end
 
-def handel_option(option, app)
-  case option
-  when 3
-    create_person(app)
-  when 4
-    app.create_book
-  when 5
-    app.create_rental
-  when 6
-    puts 'Enter person ID:-'
-    id = gets.chomp.to_i
-    app.list_person_rentals(id)
-  when 7
-    puts 'Thanks You!!...'
-    return false
-  else
-    puts 'Invalid option. Please try again.'
-  end
-  true
-end
-
-def main
-  status = true
-  app = App.new
-  while status
-    run_option
-    option = gets.chomp.to_i
-    if option == 1
-      app.list_books
-    elsif option == 2
-      app.list_peoples
+def handle_choice(choice, app)
+    choice_actions = {
+      1 => :list_all_books,
+      2 => :list_all_people,
+      3 => :create_person,
+      4 => :create_book_prompt,
+      5 => :create_rental_prompt,
+      6 => :list_rentals_for_person_prompt,
+      7 => :quit
+    }
+  
+    action = choice_actions[choice]
+  
+    if action
+      if action == :create_person
+        create_person(app)  # Pass the `app` argument
+      else
+        app.send(action)
+      end
     else
-      status = handel_option(option, app)
+      puts 'Invalid choice. Please try again.'
     end
   end
+  
+
+def main
+  app = App.new
+
+  puts 'Welcome to the Library App!'
+
+  loop do
+    print_menu
+    choice = gets.chomp.to_i
+
+    handle_choice(choice, app)
+
+    break if choice == 7
+  end
+
+  puts 'Goodbye!'
 end
 
 main
