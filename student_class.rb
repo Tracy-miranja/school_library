f012e1e2dcc4f9c516534ccdebc542d65e2dfdd6
Tracy-miranja <barazatracy44@gmail.com>
2023-05-25 21:13:19 +0300

    Correct linter error

--- student_class.rb
+++ student_class.rb
@@ -1,22 +1,3 @@
-# require_relative 'person_class'
-
-# class Student < Person
-#   attr_reader :classroom
-
-#   def initialize(age, name, parent_permission: true)
-#     super(name, age, parent_permission)
-#     @classroom = nil
-#   end
-
-#   def assign_classroom(classroom)
-#     @classroom = classroom
-#     classroom.add_student(self) unless classroom.students.include?(self)
-#   end
-
-#   def play_hooky
-#     '¯\(ツ)/¯'
-#   end
-# end
 require_relative 'person_class'
 
 class Student < Person
